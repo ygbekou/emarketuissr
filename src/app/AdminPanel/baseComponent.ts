d73a45b0138cbc80006ccf06759d21a3f8ba7b26
Panawe Batanado <panawe.batanado@ipnetinstitute.com>
2020-07-09 21:02:14 -0400

    Change

--- src/app/AdminPanel/baseComponent.ts
+++ src/app/AdminPanel/baseComponent.ts
@@ -1,17 +1,14 @@
-import {Component} from '@angular/core';
-import { TranslateService} from '@ngx-translate/core';
-import { TokenStorage } from '../token.storage';
-import { Cookie } from 'ng2-cookies/ng2-cookies';
-
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 @Component({
-	template: ``,
+  template: ``,
   providers: []
 })
 export class BaseComponent {
 
-   public messages: string;
-   public messageColor: string;
-   public hasError: boolean;
+  public messages: string;
+  public messageColor: string;
+  public hasError: boolean;
 
   constructor
     (
@@ -21,24 +18,23 @@ export class BaseComponent {
   }
 
   protected processResult(result, entityObject, pictureUrl) {
-    console.info(result)
     if (result.errors === null || result.errors.length === 0) {
       this.hasError = false;
       entityObject = result;
       this.translate.get(['COMMON.SAVE', 'MESSAGE.SAVE_SUCCESS']).subscribe(res => {
-          this.messages = res['MESSAGE.SAVE_SUCCESS'];
+        this.messages = res['MESSAGE.SAVE_SUCCESS'];
       });
-      
+
       if (entityObject.user && entityObject.user.birthDate != null) {
-          entityObject.user.birthDate = new Date(entityObject.user.birthDate);
+        entityObject.user.birthDate = new Date(entityObject.user.birthDate);
       }
       if (pictureUrl) {
-          pictureUrl = '';
+        pictureUrl = '';
       }
     } else {
       this.hasError = true;
       this.translate.get(['COMMON.SAVE', 'MESSAGE.SAVE_UNSUCCESS']).subscribe(res => {
-          this.messages = res['MESSAGE.SAVE_UNSUCCESS'] + '<br/>' + result.errors[0];
+        this.messages = res['MESSAGE.SAVE_UNSUCCESS'] + '<br/>' + result.errors[0];
       });
     }
   }
@@ -46,7 +42,6 @@ export class BaseComponent {
   removeItem(listItems: any[], id: number) {
     const index = listItems.findIndex(x => x.id === id);
     listItems.splice(index, 1);
-
   }
 
   removeElement(listItems: any[], name: string) {
@@ -54,34 +49,16 @@ export class BaseComponent {
     listItems.splice(index, 1);
   }
 
-    protected processDeleteResult(result, messages) {
+  protected processDeleteResult(result, messages) {
     if (result.result === 'SUCCESS') {
-        this.translate.get(['COMMON.DELETE', 'MESSAGE.DELETE_SUCCESS']).subscribe(res => {
-            this.messages = res['MESSAGE.DELETE_SUCCESS'];
-        });
+      this.translate.get(['COMMON.DELETE', 'MESSAGE.DELETE_SUCCESS']).subscribe(res => {
+        this.messages = res['MESSAGE.DELETE_SUCCESS'];
+      });
     } else {
-        this.translate.get(['COMMON.DELETE', 'MESSAGE.DELETE_UNSUCCESS', 'MESSAGE.' + result.errors[0]]).subscribe(res => {
-            messages = res['MESSAGE.DELETE_UNSUCCESS'] + ': ' + res['MESSAGE.' + result.errors[0]];
-        });
+      this.translate.get(['COMMON.DELETE', 'MESSAGE.DELETE_UNSUCCESS', 'MESSAGE.' + result.errors[0]]).subscribe(res => {
+        messages = res['MESSAGE.DELETE_UNSUCCESS'] + ': ' + res['MESSAGE.' + result.errors[0]];
+      });
     }
   }
 
-
-  setLang() {
-    let lang = navigator.language;
-    if (lang) {
-      lang = lang.substring(0, 2);
-    }
-    if (Cookie.get('lang')) {
-      lang = Cookie.get('lang');
-      console.log('Using cookie lang=' + Cookie.get('lang'));
-    } else if (lang) {
-      console.log('Using browser lang=' + lang);
-      // this.translate.use(lang);
-    } else {
-      lang = 'fr';
-      console.log('Using default lang=fr');
-    }
-    this.lang = lang;
-  }
- }
+}
--- src/app/Pages/UserAccount/Account/Account.component.ts
+++ src/app/Pages/UserAccount/Account/Account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-Account',
@@ -7,7 +8,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
 
-  constructor() { }
+
+   menuItems = [
+    {
+      state: 'admin/admindash',
+      name: 'Dashboard',
+      type: 'link',
+      icon: 'poll'
+    },
+    {
+      state: 'admin/invoices',
+      name: 'Invoices',
+      type: 'link',
+      icon: 'recent_actors'
+    },
+    {
+      state: 'admin',
+      name: 'Products',
+      type: 'sub',
+      icon: 'shopping_cart',
+      children: [
+        { state: 'categories', name: 'Categories', type: 'link' },
+        { state: 'products', name: 'Products', type: 'link' },
+        { state: 'product-add', name: 'Product Add', type: 'link' },
+        { state: 'information', name: 'Information', type: 'link' }
+      ]
+    },
+    {
+      state: 'admin/system',
+      name: 'System',
+      type: 'sub',
+      icon: 'shopping_cart',
+      children: [
+        { state: 'stores', name: 'Stores', type: 'link' },
+        { state: 'languages', name: 'Languages', type: 'link' },
+        { state: 'currencies', name: 'Currencies', type: 'link' },
+        { state: 'stockStatuses', name: 'Stock Statutes', type: 'link' },
+        { state: 'orderStatuses', name: 'Order Statutes', type: 'link' },
+        { state: 'returnStatuses', name: 'Return Statutes', type: 'link' },
+        { state: 'returnReasons', name: 'Return Reasons', type: 'link' },
+        { state: 'returnActions', name: 'Return Actions', type: 'link' },
+        { state: 'countries', name: 'Countries', type: 'link' },
+        { state: 'zones', name: 'Zones', type: 'link' },
+        { state: 'geoZones', name: 'Geo Zones', type: 'link' },
+        { state: 'lengthClasses', name: 'Length classes', type: 'link' },
+        { state: 'weightClasses', name: 'Weight classes', type: 'link' },
+        { state: 'taxClasses', name: 'Tax classes', type: 'link' },
+        { state: 'taxRates', name: 'Tax rates', type: 'link' }
+      ]
+    },
+    {
+      state: 'admin/account/profile',
+      name: 'Profile',
+      type: 'link',
+      icon: 'account_circle'
+    },
+    {
+      state: '/home',
+      name: 'Go To Site',
+      type: 'link',
+      icon: 'home'
+    }
+  ];
+  constructor(public translate: TranslateService) { }
 
   ngOnInit() {
   }
