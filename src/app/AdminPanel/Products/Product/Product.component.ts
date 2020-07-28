8dbb9700d71ebb0c68484fe7b2e66277594800d3
Panawe Batanado <panawe.batanado@ipnetinstitute.com>
2020-07-28 03:34:27 -0400

    Change

--- src/app/AdminPanel/Products/Product/Product.component.ts
+++ src/app/AdminPanel/Products/Product/Product.component.ts
@@ -7,83 +7,89 @@ import { ProductDescriptionComponent } from '../ProductDescription/ProductDescri
 import { BaseComponent } from '../../baseComponent';
 
 @Component({
-   selector: 'app-product',
-   templateUrl: './Product.component.html',
-   styleUrls: ['./Product.component.scss']
+  selector: 'app-product',
+  templateUrl: './Product.component.html',
+  styleUrls: ['./Product.component.scss']
 })
 
 export class ProductComponent extends BaseComponent implements OnInit {
 
-   messages = '';
+  messages = '';
 
-   @ViewChild(ProductDescriptionComponent, {static: false}) productDescriptionView: ProductDescriptionComponent;
+  @ViewChild(ProductDescriptionComponent, { static: false }) productDescriptionView: ProductDescriptionComponent;
 
-   product: Product;
+  product: Product;
 
 
-   constructor(
-     private activatedRoute: ActivatedRoute,
-      public translate: TranslateService,
-      public appService: AppService) { 
-        super(translate);
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    public translate: TranslateService,
+    public appService: AppService) {
+    super(translate);
+  }
+
+  ngOnInit() {
+
+    this.activatedRoute.params.subscribe(params => {
+      if (params.id === undefined || params.id === 0) {
+        this.clear();
+      } else {
+        this.clear();
+        this.getProductDescriptions(params.id);
       }
+    });
 
-   ngOnInit() {
+  }
 
-      this.activatedRoute.params.subscribe(params => {
-        if (params.id === undefined || params.id === 0) {
-          this.clear();
-        } else {
-          this.clear();
-          this.getProductDescriptions(params.id);
+  clear() {
+    this.product = new Product();
+
+    for (const lang of this.appService.appInfoStorage.languages) {
+
+      const pd = new ProductDescription();
+      pd.language = lang;
+      this.product.productDescriptions.push(pd);
+
+    }
+  }
+
+  getProductDescriptions(productId: number) {
+    const parameters: string[] = [];
+    if (productId != null) {
+      parameters.push('e.product.id = |productId|' + productId + '|Integer');
+    }
+    this.appService.getAllByCriteria('com.softenza.emarket.model.ProductDescription', parameters)
+      .subscribe((data: ProductDescription[]) => {
+
+        if (data !== null && data.length > 0) {
+          this.product = data[0].product;
+          this.product.productDescriptions = data;
+          this.productDescriptionView.product = this.product;
+          this.productDescriptionView.refreshLangObjects();
+
         }
-      });
-
-   }
-
-   clear() {
-     this.product = new Product();
-
-      for (const lang of this.appService.appInfoStorage.languages) {
-
-          const pd = new ProductDescription();
-          pd.language = lang;
-          this.product.productDescriptions.push(pd);
-
-        }
-   }
-
-   getProductDescriptions(productId: number) {
-      const parameters: string[] = [];
-      if (productId != null) {
-         parameters.push('e.product.id = |productId|' + productId + '|Integer');
-      }
-      this.appService.getAllByCriteria('com.softenza.emarket.model.ProductDescription', parameters)
-         .subscribe((data: ProductDescription[]) => {
-           
-          if (data !== null && data.length > 0) {
-            this.product = data[0].product;
-            this.product.productDescriptions = data;
-            this.productDescriptionView.product = this.product;
-            this.productDescriptionView.refreshLangObjects();
-            
-          }
       },
         error => console.log(error),
         () => console.log('Get all Category Item complete'));
-   }
+  }
 
-   setProductToggles() {
-      this.product.status = (this.product.status == null || this.product.status.toString() === 'false') ? 0 : 1;
-      this.product.shipping = (this.product.shipping == null || this.product.shipping.toString() === 'false') ? 0 : 1;
-      this.product.subtract = (this.product.subtract == null || this.product.subtract.toString() === 'false') ? 0 : 1;
-   }
+  setProductToggles() {
+    this.product.status = (this.product.status == null
+      || this.product.status.toString() === 'false'
+      || this.product.status.toString() === '0') ? 0 : 1;
+    this.product.shipping = (this.product.shipping == null
+      || this.product.shipping.toString() === 'false'
+      || this.product.shipping.toString() === '0') ? 0 : 1;
+    this.product.subtract = (this.product.subtract == null
+      || this.product.subtract.toString() === 'false'
+      || this.product.subtract.toString() === '0') ? 0 : 1;
+  }
 
   save() {
     this.messages = '';
     try {
       this.setProductToggles();
-      const prod = {...this.product};
+      const prod = { ...this.product };
       prod.productDescriptions = [];
 
       this.appService.save(prod, 'Product')
