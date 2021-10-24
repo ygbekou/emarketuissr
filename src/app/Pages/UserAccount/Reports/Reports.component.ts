775629d791e38da9e3fa3e9dd75fdf1f086de518
Panawe Batanado <panawe@gmail.com>
2021-10-24 05:34:30 -0400

    change

--- src/app/Pages/UserAccount/Reports/Reports.component.ts
+++ src/app/Pages/UserAccount/Reports/Reports.component.ts
@@ -134,8 +134,10 @@ export class ReportsComponent implements OnInit {
       rep.reportName = 'receipts';
     } else if (this.subRpt > 6 && this.subRpt < 9) {
       rep.reportName = 'sales';
-    } else if (this.subRpt > 8 && this.subRpt < 11) {
+    } else if (this.subRpt > 8 && this.subRpt < 10) {
       rep.reportName = 'expenses';
+    } else if (this.subRpt > 9 && this.subRpt < 11) {
+      rep.reportName = 'pos';
     }
 
     const parm1 = new Parameter('pUserId', this.appService.tokenStorage.getUserId());
