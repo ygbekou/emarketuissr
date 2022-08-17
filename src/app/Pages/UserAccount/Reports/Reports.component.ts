e23fcb1cf0e3696ee94d7f977f0e80f378338dc0
Panawe Batanado <panawe@gmail.com>
2022-08-17 00:29:04 -0400

    change

--- src/app/Pages/UserAccount/Reports/Reports.component.ts
+++ src/app/Pages/UserAccount/Reports/Reports.component.ts
@@ -174,6 +174,8 @@ export class ReportsComponent implements OnInit {
       rep.reportName = 'hotel_rooms_history';
     } else if (this.subRpt === 14) {
       rep.reportName = 'hotel_sales';
+    } else if (this.subRpt === 15) {
+      rep.reportName = 'sales';
     }
 
 
